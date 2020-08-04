ebc657f50fdc3e99e51c5797b2c46c098d2fb76d
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-08-04 16:40:13 +0200

    Update plain docker github action so that command.sh can be used in both actions

--- commands.sh
+++ commands.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -l
 
+# echo pwd `pwd`: /home/cmsusr/CMSSW_5_3_32/src
+# echo $USER cmsusr
+sudo chown $USER /mnt/vol
+
 mkdir workspace
 cd workspace
-# this is already cloned in the previous step and could be passed in the mounted volume
+# Clone done here for the compatibility with the minikube workflow 
+# For the plain github action with docker, the are would be available in /mnt/vol
 git clone git://github.com/katilp/AOD2NanoAODOutreachTool  AOD2NanoAOD
 cd AOD2NanoAOD
 scram b -j8
@@ -10,11 +15,9 @@ scram b -j8
 #cmsRun configs/simulation_cfg.py
 cmsRun configs/data_cfg.py
 
-ls -l 
+cp output.root /mnt/vol/ 
+echo  ls -l /mnt/vol 
+ls -l /mnt/vol
 
-sudo chown -R cmsusr /mountedvolume
-chmod 755 /mountedvolume
-mkdir /mountedvolume/outputs
-cp output.root /mountedvolume/outputs 
-ls -l /mountedvolume/outputs
-touch /mountedvolume/outputs/empty.out
+
+
