0d5f3791951ce80480315bb7e881ff6c7eb173cf
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-08-05 09:06:41 +0200

    Update commands (#6)
    * Update commands script
    
    * Update commands script
    
    * Update docker wf
    
    * fix commands
    
    * Update main_argo.yml

--- commands.sh
+++ commands.sh
@@ -1,22 +1,24 @@
 #!/bin/sh -l
-
+# parameters: $1 number of events, $2 configuration file
 # echo pwd `pwd`: /home/cmsusr/CMSSW_5_3_32/src
 # echo $USER cmsusr
 sudo chown $USER /mnt/vol
 
 mkdir workspace
 cd workspace
-# Clone done here for the compatibility with the minikube workflow 
-# For the plain github action with docker, the are would be available in /mnt/vol
-git clone git://github.com/katilp/AOD2NanoAODOutreachTool  AOD2NanoAOD
+# For the plain github action with docker, the area would be available in /mnt/vol
+git clone git://github.com/cms-opendata-analyses/AOD2NanoAODOutreachTool  AOD2NanoAOD
 cd AOD2NanoAOD
 scram b -j8
 
-#cmsRun configs/simulation_cfg.py
-cmsRun configs/data_cfg.py
+if [ -z "$1" ]; then nev=10000; else nev=$1; fi
+if [ -z "$2" ]; then config=data_cfg.py; else config=$2; fi
+eventline=$(grep maxEvents configs/$config)
+sed -i "s/$eventline/process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32($nev) )/g" configs/$config
+cmsRun configs/$config
 
-cp output.root /mnt/vol/ 
-echo  ls -l /mnt/vol 
+cp output.root /mnt/vol/
+echo  ls -l /mnt/vol
 ls -l /mnt/vol
 
 
